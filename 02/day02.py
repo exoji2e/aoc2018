2565582a5bcf5b2fb71ac0a6b9dd612fce73ee2a
Måns Magnusson <exoji2e@gmail.com>
2018-12-02 08:14:48 +0100

    add day02

---
+++ 02/day02.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.extend(['..', '.'])
+from fetch import fetch
+from collections import *
+
+def p1(ws):
+    x, y = 0, 0
+    for w in ws:
+        C = Counter()
+        for c in w: C[c] += 1
+        if 2 in set(C.values()): x += 1
+        if 3 in set(C.values()): y += 1
+    return x*y
+
+def p2(ws):
+    for w1 in ws:
+        for w2 in ws:
+            common = ''
+            for c1, c2 in zip(w1, w2):
+                if c1 == c2:
+                    common += c1
+            if len(common) == len(w1) - 1:
+                return common
+
+v = fetch(2).split()
+
+print('part_1: {}'.format(p1(v)))
+print('part_2: {}'.format(p2(v)))
+
+
+
+
